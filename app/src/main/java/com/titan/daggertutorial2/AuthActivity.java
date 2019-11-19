a017a819e812f2dc220ae2937347e3ce895e1f69
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-19 20:23:53 +0000

    Component Modules and Static @Provides

--- app/src/main/java/com/titan/daggertutorial2/AuthActivity.java
+++ app/src/main/java/com/titan/daggertutorial2/AuthActivity.java
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 import dagger.android.support.DaggerAppCompatActivity;
+import timber.log.Timber;
 
 public class AuthActivity extends DaggerAppCompatActivity {
+
+    @Inject
+    String sdfsdf;
+
+    @Inject
+    boolean isAppNull;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        Timber.d("onCreate: sdfsdf = " + sdfsdf);
+        Timber.d("onCreate: is app null? " + isAppNull);
     }
 }
--- app/src/main/java/com/titan/daggertutorial2/BaseApplication.java
+++ app/src/main/java/com/titan/daggertutorial2/BaseApplication.java
@@ -4,9 +4,16 @@ import com.titan.daggertutorial2.di.DaggerAppComponent;
 
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
+import timber.log.Timber;
 
 public class BaseApplication extends DaggerApplication {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Timber.plant(new Timber.DebugTree());
+    }
 
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
