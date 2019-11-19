19c6410a2aeb9af72148d871fff51c7ca60ce7a1
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-19 19:59:23 +0000

    Injecting Activities and @ContributesAndroidInjector

---
+++ app/src/main/java/com/titan/daggertutorial2/di/ActivityBuildersModule.java
@@ -0,0 +1,13 @@
+package com.titan.daggertutorial2.di;
+
+import com.titan.daggertutorial2.AuthActivity;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+}
