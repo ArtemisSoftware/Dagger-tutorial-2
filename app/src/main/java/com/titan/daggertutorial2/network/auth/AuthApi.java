1f8ea6d95421c3f2d6b0f24479e3957b66f98030
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-22 18:55:57 +0000

    Retrofit Calls and RxJava

--- app/src/main/java/com/titan/daggertutorial2/network/auth/AuthApi.java
+++ app/src/main/java/com/titan/daggertutorial2/network/auth/AuthApi.java
@@ -1,11 +1,15 @@
 package com.titan.daggertutorial2.network.auth;
 
+import com.titan.daggertutorial2.models.User;
+
+import io.reactivex.Flowable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi {
 
-    @GET
-    Call<ResponseBody> getFakeStuff();
+    @GET("users/{id}")
+    Flowable<User> getUser(@Path("id") int id);
 }
--- app/src/main/java/com/titan/daggertutorial2/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/titan/daggertutorial2/ui/auth/AuthViewModel.java
@@ -2,10 +2,14 @@ package com.titan.daggertutorial2.ui.auth;
 
 import androidx.lifecycle.ViewModel;
 
+import com.titan.daggertutorial2.models.User;
 import com.titan.daggertutorial2.network.auth.AuthApi;
 
 import javax.inject.Inject;
 
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
 
 public class AuthViewModel extends ViewModel {
@@ -17,12 +21,33 @@ public class AuthViewModel extends ViewModel {
 
         this.authApi = authApi;
         Timber.d("viewmodel is working");
+        Timber.d("auth api: " + this.authApi);
 
-        if(this.authApi == null){
-            Timber.d("auth api is NULL");
-        }
-        else{
-            Timber.d("auth api is NOT NULL");
-        }
+        authApi.getUser(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Timber.d("onNext: "+ user.getEmail());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e("onError: "+ e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
     }
 }
