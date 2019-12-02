567551336d3a99a6667e1da522398d1ac19cab16
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-02 19:25:47 +0000

    Navigation Drawer AndroidX

--- app/src/main/java/com/titan/daggertutorial2/ui/main/MainActivity.java
+++ app/src/main/java/com/titan/daggertutorial2/ui/main/MainActivity.java
@@ -7,7 +7,10 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.titan.daggertutorial2.BaseActivity;
 import com.titan.daggertutorial2.R;
 import com.titan.daggertutorial2.ui.main.posts.PostsFragments;
@@ -15,12 +18,18 @@ import com.titan.daggertutorial2.ui.main.profile.ProfileFragment;
 
 import timber.log.Timber;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         Timber.d("creating...");
         testFragment();
@@ -48,5 +57,29 @@ public class MainActivity extends BaseActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+
+            case R.id.nav_profile:{
+
+                break;
+            }
+
+            case R.id.nav_posts:{
+
+                break;
+            }
+
+
+        }
+
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
